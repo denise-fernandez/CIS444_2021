a15a29d6f33bd7d248d20c705d75c107836f17a3
Ubuntu <ubuntu@ip-172-31-0-234.us-west-1.compute.internal>
2021-11-22 20:14:38 +0000

    getting signup to work

--- HW4/flask_jwt_rest_server/open_calls/signup.py
+++ HW4/flask_jwt_rest_server/open_calls/signup.py
@@ -1,47 +1,36 @@
-from flask import Flask,render_template,request, redirect, url_for, g
+from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
-import jwt
-
-import sys
-import datetime
+from tools.token_tools import create_token
 import bcrypt
-import traceback
-
-
-from db_con import get_db_instance, get_db
-
-from tools.token_required import token_required
-from tools.get_aws_secrets import get_secrets
+import psycopg2
+from psycopg2 import sql
 
 from tools.logging import logger
-global JWT
 
 def handle_request():
     logger.debug("Signup Handle Request")
-
-    password = request.form['password']
-    username = request.form['username']
+    #use data here to auth the user
+    password_from_user = request.form['password']
+    ufu = request.form['username']
 
     cur = g.db.cursor()
+    cur.execute(sql.SQL("select * from users where username = '" + ufu + "';"))
+    user = cur.fetchone()
+    cur.close()
 
-    user = {
-            "sub" : request.form['username'] #sub is used by pyJwt as the owner of the token
-            }
+    logger.debug(user)
 
-    cur.execute("select username from users where username = '" + username + "';")
-    userid = cur.fetchone()
-
-    if userid == username:
+    if user is not None:
         print("That username already exists, try another")
-        return json_response(data={"message:" + username + " already exists!"})
-    else:
-        saltedpassword = bcrypt.hashpw( bytes(password, 'utf-8'), bcrypt.gensalt(10))
+        return json_response(message = "User already exists")
 
-        cur.execute("insert into users (username, password) VALUES ('"
-                    + username + "', '" + saltedpassword.decode('utf-8') + "');", (user['sub'], saltedpassword.decode('utf-8')))
+    else:
+        saltedpassword = bcrypt.hashpw( bytes(password_from_user, 'utf-8'), bcrypt.gensalt(12))
+        logger.debug(saltedpassword)
+        cur.execute(sql.SQL("insert into users (username, password) VALUES ('"
+                    + user + "', '" + saltedpassword.decode('utf-8') + "');"))
         cur.close()
         g.db.commit()
 
-        print("Created the following user: " + username)
-
-        return json_response(data={"message": username +" created successfully."})
+        logger.debug("Created the following user: " + user)
+        return json_response(message = "Successfully created user")
--- HW4/flask_jwt_rest_server/secure_calls/get_books.py
+++ HW4/flask_jwt_rest_server/secure_calls/get_books.py
@@ -1,10 +1,28 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
-
+from psycopg2 import sql
+import json
 from tools.logging import logger
 
 def handle_request():
     logger.debug("Get Books Handle Request")
+    in_jwt = request.args.get("jwt")
+ 
+    cur = g.db.cursor()
 
-    return json_response( token = create_token(  g.jwt_data ) , books = {})
+    cur.execute("select * from books;")
+    books_in_db = cur.fetchall()
+    cur.close()
+
+    message = '{"books":['
+    for b in books_in_db:
+        if b[0] < len(books_in_db) :
+            message += '{"id":"'+str(b[0]) + '","name":"' + str(b[1]) + '","price":"' + str(b[2]) +'"},'
+        else:
+            message += '{"id":"'+str(b[0]) + '","name":"' + str(b[1]) + '","price":"' + str(b[2]) +'"}'
+    message += "]}"
+    logger.debug(message)
+    books= books_in_db
+    logger.debug(books)
+    return json_response( token = create_token(  g.jwt_data ), data = json.loads(message) )
