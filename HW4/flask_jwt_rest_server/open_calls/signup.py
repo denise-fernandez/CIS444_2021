1bf75cf384c47689aefaf2d1f9deb7eb5ad9320d
Ubuntu <ubuntu@ip-172-31-0-234.us-west-1.compute.internal>
2021-11-22 20:45:10 +0000

    done!

--- HW4/flask_jwt_rest_server/open_calls/signup.py
+++ HW4/flask_jwt_rest_server/open_calls/signup.py
@@ -1,22 +1,29 @@
-from flask import request, g
+from flask import Flask,render_template,request, redirect, url_for, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
-from tools.token_tools import create_token
-import bcrypt
+import jwt
 import psycopg2
 from psycopg2 import sql
 
+import sys
+import datetime
+import bcrypt
+import traceback
+
+
+from db_con import get_db_instance, get_db
+from tools.token_tools import create_token
+
 from tools.logging import logger
+
 
 def handle_request():
     logger.debug("Signup Handle Request")
-    #use data here to auth the user
-    password_from_user = request.form['password']
-    ufu = request.form['username']
+    password_from_user = request.form['newpassword']
+    ufu = request.form['newuser']
 
     cur = g.db.cursor()
     cur.execute(sql.SQL("select * from users where username = '" + ufu + "';"))
     user = cur.fetchone()
-    cur.close()
 
     logger.debug(user)
 
@@ -28,9 +35,9 @@ def handle_request():
         saltedpassword = bcrypt.hashpw( bytes(password_from_user, 'utf-8'), bcrypt.gensalt(12))
         logger.debug(saltedpassword)
         cur.execute(sql.SQL("insert into users (username, password) VALUES ('"
-                    + user + "', '" + saltedpassword.decode('utf-8') + "');"))
+                    + ufu + "', '" + saltedpassword.decode('utf-8') + "');"))
         cur.close()
         g.db.commit()
 
-        logger.debug("Created the following user: " + user)
+        logger.debug("Created the following user: " + ufu)
         return json_response(message = "Successfully created user")
